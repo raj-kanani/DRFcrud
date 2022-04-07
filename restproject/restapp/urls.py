912913c5ab3abc55c61eacdd6b0bf1576e29b228
raj-kanani <raj.kanani@plutustec.com>
2022-04-07 14:16:41 +0530

    deserialization

--- restproject/restapp/urls.py
+++ restproject/restapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('Api/', views.StudentApi.as_view(), name='Api'),
-    # path('Api2/', views.hello_world, name='Api2'),
+    path('student/', views.StudentApi.as_view(), name='student'),
+    path('hello/', views.hello_world, name='hello'),
 
 ]
